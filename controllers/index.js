d6530836f5b2df4f2d422c9e9bed7e7dfcf257b6
edoridha <edo.ridha@gmail.com>
2023-07-06 21:27:13 +0700

    session&middleware

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-const User = require('../models')
+const {User} = require('../models')
 const bcrypt = require('bcryptjs');
 
 class Controller {
@@ -16,16 +16,25 @@ class Controller {
                 if (user) {
                     if (user.role == 'Admin') {
                         const isValidPass = bcrypt.compareSync(password, user.password)
-                        if (isValidPass) return res.redirect('AdminDashboard')
+                        if (isValidPass) {
+                            req.session.userRole = user.role
+                            return res.redirect('/admin')
+                        }
                         return res.redirect('/login?error=invalid password')
                     } else {
                         const isValidPass = bcrypt.compareSync(password, user.password)
-                        if (isValidPass) return res.redirect('AdminDashboard')
+                        if (isValidPass) {
+                            req.session.userRole = user.role
+                            return res.redirect('/admin')
+                        }
                         return res.redirect('/login?error=invalid password')
                     }
                 } else return res.redirect('/login?error=invalid email')
             })
             .catch(err => console.log(err))
+    }
+    static showAdmin(req, res) {
+        res.render('AdminDashboard')
     }
 }
 
--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,28 @@
 const Controller = require('../controllers');
 const router = require('express').Router()
+const user = require('./user');
+
 
 router.get('/', Controller.home)
 router.get('/login', Controller.login)
 router.post('/login', Controller.postLogin)
 router.get('/register')
 router.post('/register')
-router.get('/admin')
+// router.use(require('/user', user))
+
+router.use(function(req, res, next){
+    if(!req.session.userRole || req.session.userRole !== 'Admin'){
+        const error = 'Please login with Admin user'
+       return res.redirect(`/login?error=${error}`, {error})
+    }
+    next()
+})
+
+router.get('/admin', Controller.showAdmin)
+router.get('/admin/addCource')
+router.post('/admin/addCourse')
+router.get('/admin/edit/:id')
+router.post('/admin/edit/:id')
+router.get('/admin/delete/:id')
 
 module.exports = router;
