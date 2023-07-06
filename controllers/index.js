ff4a6710160e2dada178690a400795cbaa879b28
edoridha <edo.ridha@gmail.com>
2023-07-06 19:28:35 +0700

    update edo

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,31 @@
-class Controller{
+const User = require('../models')
+const bcrypt = require('bcryptjs');
+
+class Controller {
 
     static home(req, res) {
-        res.send("Hallo")
+        res.render('Home')
+    }
+    static login(req, res) {
+        res.render('Login')
+    }
+    static postLogin(req, res) {
+        const { email, password } = req.body
+        User.findOne({ where: { email } })
+            .then(user => {
+                if (user) {
+                    if (user.role == 'Admin') {
+                        const isValidPass = bcrypt.compareSync(password, user.password)
+                        if (isValidPass) return res.redirect('AdminDashboard')
+                        return res.redirect('/login?error=invalid password')
+                    } else {
+                        const isValidPass = bcrypt.compareSync(password, user.password)
+                        if (isValidPass) return res.redirect('AdminDashboard')
+                        return res.redirect('/login?error=invalid password')
+                    }
+                } else return res.redirect('/login?error=invalid email')
+            })
+            .catch(err => console.log(err))
     }
 }
 
--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,11 @@
 const Controller = require('../controllers');
 const router = require('express').Router()
 
-router.use('/', Controller.home)
-
+router.get('/', Controller.home)
+router.get('/login', Controller.login)
+router.post('/login', Controller.postLogin)
+router.get('/register')
+router.post('/register')
+router.get('/admin')
 
 module.exports = router;
