4d562f940976eb5804fc2aaff4cd576617ba2434
afuadlabib <afuad.lab@gmail.com>
2023-07-06 13:50:01 +0700

    add

---
+++ seeders/20230706052529-category.js
@@ -0,0 +1,22 @@
+'use strict';
+const fs = require('fs')
+
+const data = JSON.parse(fs.readFileSync('./data/categories.json', 'utf-8'))
+.map((e) => {
+    e.createdAt = new Date();
+    e.updatedAt = new Date();
+    return e
+  })
+  console.log(data)
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Categories", data)
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Categories', null);
+
+  }
+};
